6c5269bf42cfa7afa82ed80876b3b04cf6eefee3
Kirill Shikhalev <esbraff@yandex.ru>
2020-02-18 22:51:51 +0300

    Updates

---
+++ precache-manifest.d0d46e5b6263310a9476ef2892753457.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1da9b300d3f9831a9d87f22f27256ae",
+    "url": "/dotizer/index.html"
+  },
+  {
+    "revision": "fc641d0adbbae216d7a6",
+    "url": "/dotizer/static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "42dbd137b64cc166091a",
+    "url": "/dotizer/static/css/main.e8b019ea.chunk.css"
+  },
+  {
+    "revision": "fc641d0adbbae216d7a6",
+    "url": "/dotizer/static/js/2.fa8e6fee.chunk.js"
+  },
+  {
+    "revision": "041a2eef9c260e294d2b14dcf6c1c7b9",
+    "url": "/dotizer/static/js/2.fa8e6fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dbd137b64cc166091a",
+    "url": "/dotizer/static/js/main.fb8fd59c.chunk.js"
+  },
+  {
+    "revision": "32e62fb95b89339f5e34",
+    "url": "/dotizer/static/js/runtime-main.eebe4081.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/dotizer/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
